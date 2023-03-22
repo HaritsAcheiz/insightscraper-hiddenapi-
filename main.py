ada306948ea0230ec68f82c99d2b98fb8213b6d8
Haritz <harits.muhammad.only@gmail.com>
2023-03-23 06:45:27 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,77 @@
+import httpx
+from dataclasses import dataclass, field
+from typing import List
+from random import choice
+
+@dataclass
+class Scraper:
+    useragent: List[str] = field(default_factory=lambda: [
+        'Mozilla/5.0 (Wayland; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.137 Safari/537.36 Ubuntu/22.04 (5.0.2497.35-1) Vivaldi/5.0.2497.35',
+        'Mozilla/5.0 (Wayland; Linux x86_64; System76 Galago Pro (galp2)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.175 Safari/537.36 Ubuntu/22.04 (5.0.2497.48-1) Vivaldi/5.0.2497.48',
+        'Mozilla/5.0 (Wayland; Linux x86_64; System76 Galago Pro (galp2)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.175 Safari/537.36 Ubuntu/22.04 (5.0.2497.51-1) Vivaldi/5.0.2497.51,',
+        'Mozilla/5.0 (Wayland; Linux x86_64; System76) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.133 Safari/537.36 Ubuntu/22.04 (5.2.2623.34-1) Vivaldi/5.2.2623.39',
+        'Mozilla/5.0 (Wayland; Linux x86_64; System76) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.92 Safari/537.36 Ubuntu/22.04 (5.2.2623.34-1) Vivaldi/5.2.2623.34'
+    ])
+    def fetch(self):
+        ua = choice(self.useragent)
+
+        search_url = 'https://www.insight.com/en_US/search.html'
+
+        search_headers = {
+            'User-Agent': ua,
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
+            'TE': 'trailers'
+        }
+
+        session_url = 'https://www.insight.com/insightweb/headerInformation'
+
+        session_headers = {
+            'User-Agent': ua,
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Referer': 'https://www.insight.com/en_US/search.html',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'TE': 'trailers'
+        }
+
+        result_headers = {
+            'User-Agent': ua,
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Referer': 'https://www.insight.com/en_US/search.html',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'TE': 'trailers'
+        }
+
+        session = httpx.Client()
+        response = session.get(search_url, headers=search_headers)
+        if response.status_code != 200:
+            response.raise_for_status()
+        response = session.get(session_url, headers=session_headers)
+        session_id = response.json()['sessionId']
+        result_url = f'https://www.insight.com/api/product-search/search?country=US&instockOnly=false&lang=en_US&rows=100&start=0&salesOrg=2400&sessionId={session_id}'
+        response = session.get(result_url, headers=result_headers)
+        result = response.json()['products']
+        response.close()
+        return result
+
+if __name__ == '__main__':
+    s=Scraper()
+    result = s.fetch()
+    print(result)
